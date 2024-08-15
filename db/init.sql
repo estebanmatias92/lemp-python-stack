96fb0778639f137e41307bafd1c6c550975c976b
estebanmatias92 <estebanmatias92@gmail.com>
2024-08-15 04:43:47 -0300

    Removing vscode container settings/files

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,5 @@
 -- Create the database
-CREATE DATABASE IF NOT EXISTS todoapp;
+CREATE DATABASE IF NOT EXISTS test_db;
 
 -- Enter the database
-USE todoapp;
-
--- Creata a table inside the DB
-CREATE TABLE IF NOT EXISTS tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    is_complete BOOLEAN DEFAULT false,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-) ENGINE=INNODB;
+USE test_db;
